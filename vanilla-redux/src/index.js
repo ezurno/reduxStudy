b1d3ebf98b2efa8dc7e712ff18c6643e3c824e85
ezurno <ezurno@gmail.com>
2023-01-05 20:38:21 +0900

    countStore, reducer
    1. reducer로 value 관리
    
    2. reducer 내 action을 dispatch로 등록해 사용 (Object)
    
    3. .getState() 함수로 값을 가져와 사용

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,1 +1,44 @@
+import { createStore } from "redux";
+
 //vanilla JS redux
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const number = document.querySelector("h1");
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "PLUS") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else return count;
+};
+// redux 내에선 reducer 라고 부르는 value를 감독하는 함수
+
+const updateNumber = (value) => {
+  number.innerText = value;
+};
+
+const countStore = createStore(countModifier);
+// createStore로 count 값을 저장하는 Store를 생성
+
+countStore.dispatch({ type: "PLUS" });
+// countStore의 action.type에 ADD
+// dispatch는 Object 타입으로 값을 주어야 함
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+
+console.log(countStore.getState());
+//countStore의 State 값 가져오기
+
+plus.addEventListener("click", () => {
+  countStore.dispatch({ type: "PLUS" });
+  console.log(countStore.getState());
+  updateNumber(countStore.getState());
+});
+
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: "MINUS" });
+  console.log(countStore.getState());
+  updateNumber(countStore.getState());
+});
