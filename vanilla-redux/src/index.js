db896fac5cb9326215be06c7b7f9971fbe01c85d
ezurno <ezurno@gmail.com>
2023-01-06 23:43:39 +0900

    vanilla redux todo
    *주석 참조*

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,44 +1,92 @@
 import { createStore } from "redux";
 
-//vanilla JS redux
-const plus = document.querySelector("#plus");
-const minus = document.querySelector("#minus");
-const number = document.querySelector("h1");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const countModifier = (count = 0, action) => {
-  if (action.type === "PLUS") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else return count;
-};
-// redux 내에선 reducer 라고 부르는 value를 감독하는 함수
+const ADD_TODO = "add_todo";
+const DEL_TODO = "del_todo";
 
-const updateNumber = (value) => {
-  number.innerText = value;
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
 };
 
-const countStore = createStore(countModifier);
-// createStore로 count 값을 저장하는 Store를 생성
+const delToDo = (id) => {
+  return {
+    type: DEL_TODO,
+    id,
+  };
+};
+// reducer에 값을 넘겨주기 위한 함수들.
+// (object 변환 함수, action은 object type 이어야 하기 떄문)
 
-countStore.dispatch({ type: "PLUS" });
-// countStore의 action.type에 ADD
-// dispatch는 Object 타입으로 값을 주어야 함
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    //redux 에서는 mutation 을 절대 하지 말고 새로운 배열을 만들어야 함 !!!
+    //값이 추가 되는걸 순서를 변경하기 위해서는 ...state 순서를 변경하기
+    case DEL_TODO:
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
+    // JS의 filter 함수는 배열 속에 특정 함수를 만족하는
+    // index만 골라서 새로운 배열을 만듬
+    default:
+      return state;
+  }
+};
 
-console.log(countStore.getState());
-//countStore의 State 값 가져오기
+const store = createStore(reducer);
 
-plus.addEventListener("click", () => {
-  countStore.dispatch({ type: "PLUS" });
-  console.log(countStore.getState());
-  updateNumber(countStore.getState());
-});
+//subScribe 함수는 store 내 value를 화면에 나타내기 위함
+store.subscribe(() => {
+  console.log(store.getState());
+}); // store 에 저장 된 값 가져오기
 
-minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
-  console.log(countStore.getState());
-  updateNumber(countStore.getState());
-});
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+  //addToDo에 add 할 text 값을 args로 넘겨서 dispath
+};
+
+const dispatchDelToDo = (text) => {
+  const id = parseInt(text.target.parentNode.id);
+  //id 값을 parseInt로 int 로 변경하고 id의 부모 노드 속 id <li> 값을 가져옴
+  store.dispatch(delToDo(id));
+  // 가져온 id 값을 delToDo(), dispatch로 action 기입
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  //store 에 저장된 배열 값 불러오기
+  ul.innerHTML = "";
+  // update 시 ul 속 html을 지워서 중복 되지 않게 하기 위함
+
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("Button");
+    btn.innerText = "DELETE";
+    btn.addEventListener("click", dispatchDelToDo);
+
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+
+    li.appendChild(btn);
+    ul.appendChild(li);
+    //ul속 li 추가, li 속 btn 추가
+  });
+}; //toDo를 그려주는 함수
+
+store.subscribe(paintToDos);
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+}; // 제출 시 input tag 값 지워주고 값 전송
+
+form.addEventListener("submit", onSubmit);
+// form 에 eventListener 추가
