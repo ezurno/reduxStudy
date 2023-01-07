efd6365c2a7b424ffc67560ded57b46fca5789a2
ezurno <ezurno@gmail.com>
2023-01-07 19:12:38 +0900

    mapStateToProps()
    1. mapStateToProps()로 store의 state 값을 가져옴
    
    2. connect 함수를 currying 하여 사용함 (ES6)
    
    3. rendering 시 <Provider>를 사용해 store를 연결 해 주어야 함

--- vanilla-redux/src/routes/Home.js
+++ vanilla-redux/src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-export default function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
 
   const onChange = (event) => {
@@ -20,6 +21,14 @@ export default function Home() {
         <input type="text" value={text} onChange={onChange}></input>
         <button>Submit</button>
       </form>
+      <ul>{JSON.stringify(toDos)}</ul>
     </div>
   );
 }
+
+function mapStateToProps(state) {
+  return { toDos: state };
+} // redux reducer로 부터 현재 state 값을 가져옴
+
+export default connect(mapStateToProps)(Home);
+// store에 Home 연결. currying 해야 함.
